f29c8b789bce942e787eea08a6e0f262308eaba0
Josiah Bruner (:JosiahOne) <jsbruner@umich.edu>
2021-11-24 17:49:16 -0500

    Adds support for ECS in Terraform. Fixes #7

--- deploy/main.tf
+++ deploy/main.tf
@@ -12,11 +12,53 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_ecr_repository" "python-ast-explorer" {
   name                 = "python-ast-explorer"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+}
+
+resource "aws_ecs_task_definition" "python-ast-explorer-service" {
+  family = "python-ast-explorer"
+  container_definitions = jsonencode([
+    {
+      name      = "python-ast-explorer"
+      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-west-2.amazonaws.com/python-ast-explorer:latest"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-2a]"
+  }
+}
+
+resource "aws_ecs_cluster" "python-ast-explorer-cluster" {
+  name = "python-ast-explorer"
+}
+
+resource "aws_ecs_service" "python-ast-explorer-service" {
+  name            = "python-ast-explorer"
+  cluster         = aws_ecs_cluster.python-ast-explorer-cluster.id
+  task_definition = aws_ecs_task_definition.python-ast-explorer-service.arn
+  desired_count   = 1
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-2a]"
   }
 }
